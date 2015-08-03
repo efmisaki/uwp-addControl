a2a31d9ef38c1a4e9bb9c19fb48ac29ce3601773
Misaki <oshima+jobis@efourtion.jp>
2015-08-03 13:57:23 +0900

    uwp-addControl

---
+++ uwp-addControl/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace uwp_addControl
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void mainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Button btn = new Button();
+            btn.Content = "追加されたボタン";
+            btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+            btn.Padding = new Thickness(0, 10, 0, 10);
+            mainGrid.Children.Add(btn);
+            btn.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("ボタンイベント");     
+        }
+    }
+}
